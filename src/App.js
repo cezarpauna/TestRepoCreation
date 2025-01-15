332bd36b52a439d52ad7d0944623f259eb63db6b
cezarpauna <42316548+cezarpauna@users.noreply.github.com>
2025-01-15 12:07:51 +0200

    Add main App component for the Coffee Shop

---
+++ src/App.js
@@ -0,0 +1,15 @@
+import React from 'react';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+        <h1>Welcome to Our Local Coffee Shop!</h1>
+        <p>Enjoy the best coffee in town.</p>
+      </header>
+    </div>
+  );
+}
+
+export default App;
