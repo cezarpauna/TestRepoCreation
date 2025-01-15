63951dc525533d6e1df8deba8921cd47cb43fcd0
cezarpauna <42316548+cezarpauna@users.noreply.github.com>
2025-01-15 12:07:59 +0200

    Initial commit with simple React app

--- src/App.js
+++ src/App.js
@@ -5,8 +5,8 @@ function App() {
   return (
     <div className="App">
       <header className="App-header">
-        <h1>Welcome to Our Local Coffee Shop!</h1>
-        <p>Enjoy the best coffee in town.</p>
+        <h1>Welcome to Our Coffee Shop!</h1>
+        <p>We serve the best coffee in town.</p>
       </header>
     </div>
   );
